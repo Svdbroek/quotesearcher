ac3576b7adaa57d6f7e2f0cca74d913ae9dc2524
Svdbroek <s_in_the_water@hotmail.com>
2019-11-23 12:57:24 +0100

    still working on searchbar, can now unlike a quote

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,32 +1,60 @@
-import React, { Component } from 'react';
-import "./Quote.css"
+import React, { Component } from "react";
+import "./Quote.css";
 
 class Quote extends Component {
-    state ={
-        liked: null
-    }
-likeHandler (){
- this.setState({liked:true})
-}
+  state = {
+    liked: null ,
+  };
+  likeHandler() {
+    this.props.likeCounter(true, this.state.liked)
 
-dislikeHandler(){
-    this.setState({liked:false})
-}
+    if(this.state.liked){
+    this.setState({ liked: null })} else {this.setState({liked: true})} ;
+  }
 
-    render() {
-        return (
-            <div className ={(() => {
-                switch (this.state.liked) {
-                  case  true:   return "liked";
-                  case false: return "disliked";
-                  default:      return "n/a";
-                }
-              })()}>
-                <p>{this.props.quote}</p>
-                <p>By: {this.props.author} <button onClick={() =>{this.likeHandler()}}>:)</button> <button onClick={()=>{this.dislikeHandler()}}>:(</button></p> 
-            </div>
-        );
-    }
+  dislikeHandler() {
+    this.props.likeCounter(false, this.state.liked)
+    
+    if(this.state.liked === false){
+    this.setState({ liked: null })} else this.setState({ liked: false });
+
+  }
+
+  render() {
+    return (
+      <div
+        className={(() => {
+          switch (this.state.liked) {
+            case true:
+              return "liked";
+            case false:
+              return "disliked";
+            default:
+              return "n/a";
+          }
+        })()}
+      >
+        <p>{this.props.quote}</p>
+        <p>
+          By: {this.props.author}{" "}
+          <button
+            onClick={() => {
+              this.likeHandler();
+            }}
+          >
+            :)
+          </button>{" "}
+          <button
+            onClick={() => {
+              this.dislikeHandler();
+            }}
+          >
+            :(
+          </button>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Quote;
--- src/Components/QuoteSearcher.js
+++ src/Components/QuoteSearcher.js
@@ -5,19 +5,21 @@ class QuoteSearcher extends Component {
   state = {
     quotes: [],
     loading: true,
-    likes: 0,
-    dislikes: 0,
+    ups: 0,
+    downs: 0,
+    query: ""
   };
 
   componentDidMount() {
       console.log(this.state.loading);
-    this.Fetcher();
+    this.Fetcher("word");
     console.log(this.state.loading)
+    console.log(this.state.ups)
 
   }
 
-  Fetcher() {
-    fetch("https://quote-garden.herokuapp.com/quotes/search/tree") // make the request
+  Fetcher(query) {
+    fetch(`https://quote-garden.herokuapp.com/quotes/search/${query}`) // make the request
       .then(response => response.json())
       .then(myJson => {
         this.setState({ quotes: myJson.results, loading: false });
@@ -26,23 +28,56 @@ class QuoteSearcher extends Component {
       });
   }
 
-  likeCounter(){
+  likeCounter =( DisLiked, WasDisLiked)=>{
+    let likes = this.state.ups
+    let dislikes = this.state.downs 
+    
+    if (DisLiked === WasDisLiked){likes = 0; dislikes=0}else{ // I didn't do this the same as was suggested in the reader, because I started working on this before reading the rest of the reader
+    switch (WasDisLiked) {
+        case true:
+        if (DisLiked){ likes = likes+1; dislikes=dislikes-1 } 
+        if (!DisLiked){dislikes=dislikes+1; likes=likes-1}
+            break;
+        case false:
+        if (DisLiked){ likes = likes+1; dislikes=dislikes-1 } 
+        if (!DisLiked){dislikes=dislikes+1; likes=likes-1}
+        break;
+        default: 
+        if (DisLiked){ likes = likes+1 } 
+        if (!DisLiked){dislikes=dislikes+1}
 
+            break;
+    }}
+
+
+     this.setState({ups:likes, downs:dislikes})
   }
+
+
+  SearchChangeHandler = event => {
+    this.setState({
+      query: event.target.value
+    });
+  };
 
   render() {
     const quotes = this.state.quotes;
     return (
       <div>
-          <h2>Likes: Dislikes:</h2>
+          <h2>Likes: {this.state.ups} Dislikes:{this.state.downs} </h2>
+    <input placeholder={"Search Quotes"} onChange={this.SearchChangeHandler} value={this.state.query}/>
+
         {!this.state.loading
           ? quotes.map(quote => {
               return (
                 <Quote
+                key ={quote._id}
                   id={quote._id}
                   quote={quote.quoteText}
                   author={quote.quoteAuthor}
-                  likeCounter = {this.likeCounter()}
+                  likeCounter = {this.likeCounter}
+                  ups = {this.state.ups}
+                  downs ={this.state.downs}
                 />
               );
             })
